3d2480c785bd342aedcc94a54b8c9d1de7cc7db7
renato <renato.damazio@kzas.ai>
2020-11-10 21:57:43 -0300

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'}
     ],
     script: [
